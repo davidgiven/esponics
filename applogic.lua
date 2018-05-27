f0a842effc3a59c30a9c4b16f381c4fb024f9319
David Given <dg@cowlark.com>
2018-05-27 19:57:04 +0200

    Turn everything on on starup for a while.

--- applogic.lua
+++ applogic.lua
@@ -1,9 +1,8 @@
-fan_on()
-pump_off()
-
 local fogger_wait = tmr.create()
 local fogger_run = tmr.create()
-fogger_wait:alarm(1000, tmr.ALARM_AUTO,
+message(2, "Fogger startup")
+fogger_on()
+fogger_wait:alarm(10*1000, tmr.ALARM_AUTO,
     function()
         fogger_wait:interval(60*1000)
         message(2, "Running fogger")
@@ -18,7 +17,9 @@ fogger_wait:alarm(1000, tmr.ALARM_AUTO,
 
 local pump_wait = tmr.create()
 local pump_run = tmr.create()
-pump_wait:alarm(1000, tmr.ALARM_AUTO,
+pump_on()
+message(3, "Pump startup")
+pump_wait:alarm(30*1000, tmr.ALARM_AUTO,
     function()
         pump_wait:interval(11*60*1000)
         message(3, "Running pump")
@@ -33,7 +34,9 @@ pump_wait:alarm(1000, tmr.ALARM_AUTO,
 
 local fan_wait = tmr.create()
 local fan_run = tmr.create()
-fan_wait:alarm(1000, tmr.ALARM_AUTO,
+message(4, "Fan startup")
+fan_on()
+fan_wait:alarm(10*1000, tmr.ALARM_AUTO,
     function()
         fan_wait:interval(10*60*1000)
         message(4, "Running fan")
@@ -45,5 +48,3 @@ fan_wait:alarm(1000, tmr.ALARM_AUTO,
                 fan_off()
             end)
     end)
-
-message(2, "Waiting for startup")
