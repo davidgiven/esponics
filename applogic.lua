43626c3a0a2cd676a3be78d2194c56227a146aa4
David Given <dg@cowlark.com>
2018-05-27 18:52:59 +0200

    Added pump support. Rationalised the timers.

--- applogic.lua
+++ applogic.lua
@@ -1,22 +1,49 @@
-local CYCLE_INTERVAL = 60000
-local FOGGER_ON_TIME = 30000
-
 fan_on()
+pump_off()
 
-local timer = tmr.create()
-local t = tmr:create()
-timer:register(CYCLE_INTERVAL, tmr.ALARM_AUTO,
+local fogger_wait = tmr.create()
+local fogger_run = tmr.create()
+fogger_wait:alarm(1000, tmr.ALARM_AUTO,
     function()
+        fogger_wait:interval(60*1000)
         message(2, "Running fogger")
         fogger_on()
 
-        t:alarm(FOGGER_ON_TIME, tmr.ALARM_SINGLE,
+        fogger_run:alarm(5*1000, tmr.ALARM_SINGLE,
             function()
                 message(2, "Fogger off")
                 fogger_off()
-                fan_on()
+            end)
+    end)
+
+local pump_wait = tmr.create()
+local pump_run = tmr.create()
+pump_wait:alarm(1000, tmr.ALARM_AUTO,
+    function()
+        pump_wait:interval(11*60*1000)
+        message(3, "Running pump")
+        pump_on()
+
+        pump_run:alarm(2*1000, tmr.ALARM_SINGLE,
+            function()
+                message(3, "Pump off")
+                pump_off()
+            end)
+    end)
+
+local fan_wait = tmr.create()
+local fan_run = tmr.create()
+fan_wait:alarm(1000, tmr.ALARM_AUTO,
+    function()
+        fan_wait:interval(10*60*1000)
+        message(4, "Running fan")
+        fan_on()
+
+        fan_run:alarm(4*1000, tmr.ALARM_SINGLE,
+            function()
+                message(4, "Fan off")
+                fan_off()
             end)
     end)
 
 message(2, "Waiting for startup")
-timer:start()
--- relays.lua
+++ relays.lua
@@ -1,5 +1,7 @@
+gpio.mode(6, gpio.OUTPUT, gpio.FLOAT)
 gpio.mode(7, gpio.OUTPUT, gpio.FLOAT)
 gpio.mode(8, gpio.OUTPUT, gpio.FLOAT)
+gpio.write(6, 1)
 gpio.write(7, 1)
 gpio.write(8, 1)
 
@@ -11,10 +13,17 @@ function fan_onoff(state)
     gpio.write(7, state)
 end
 
+function pump_onoff(state)
+    gpio.write(6, state)
+end
+
 function fogger_on()  fogger_onoff(1) end
 function fogger_off() fogger_onoff(0) end
 function fan_on()     fan_onoff(1) end
 function fan_off()    fan_onoff(0) end
+function pump_on()    pump_onoff(1) end
+function pump_off()   pump_onoff(0) end
 
 fogger_off()
 fan_off()
+pump_off()
